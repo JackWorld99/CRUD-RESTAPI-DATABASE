ada06dbe024bae09c4a213568eb5564ac49faa50
JackWorld <jackwong0326@gmail.com>
2022-09-20 03:44:11 +0800

    Create model post collections.

--- Mysql/models/connect.js
+++ Mysql/models/connect.js
@@ -18,12 +18,11 @@ connection.connect((error) => {
 
         // Create Table
         // const sql = `CREATE TABLE IF NOT EXISTS posts (
-        //     id BIGINT UNSIGNED AUTO_INCREMENT,
+        //     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         //     title VARCHAR(255) NOT NULL,
         //     body VARCHAR(255) NOT NULL,
         //     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
         //     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        //     PRIMARY KEY (id)
         // )ENGINE=InnoDB`
         // const message = "Table created successfully!"
 
